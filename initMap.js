ead8d2978c96dc81847babd2a08247b14d1c01aa
DESKTOP-7QT3942\Vladik <SirnikVladik@gmail.com>
2018-05-06 19:47:34 +0300

    Function added to vue

--- initMap.js
+++ initMap.js
@@ -1,86 +1,100 @@
 var vue = new Vue({
-    el: '#result',
+    el: 'main',
     data: {
         perKilomiters: 0.185,
         perMinutes: 0.025,
         reservation: 0.9,
-        dist: '0 km',
-        time: '0 min.',
-        distValue: 0,
-        timeValue: 0,
+        dist: 0,
+        time: 0,
         cost: 1.65,
+        distanceMatrixRequest: {
+            origins: '',
+            destinations: '',
+        },
     },
     methods: {
         calculateCost() {
-            this.cost = this.reservation + this.distValue * this.perKilomiters * 0.001 + this.timeValue * this.perMinutes * 0.001;
-            this.cost = Math.ceil(this.cost * 100) / 100;
-            if (this.cost < 1.65) {
-                this.cost = 1.65;
-            };
+            let start = document.getElementById('thirstAddress').value;
+            let end = document.getElementById('secondAddress').value;
+            console.info('Получение растояния...');
+            // получение расстояния
+            var geocoder = new google.maps.Geocoder;
+            var service = new google.maps.DistanceMatrixService;
+            var bounds = new google.maps.LatLngBounds;
+
+            service.getDistanceMatrix({
+                origins: ['Чюрлёниса 7, Минск'],
+                destinations: ['Тракторный завод, Минск'],
+                travelMode: 'DRIVING',
+            }, function (response, status) {
+                if (status !== 'OK') {
+                    console.error('Error was: ' + status);
+                } else {
+                    var originList = response.originAddresses;
+                    var destinationList = response.destinationAddresses;
+
+                    ///Функция для отображения маркеров на карте
+
+                    /*var showGeocodedAddressOnMap = function () {
+                        return function (results, status) {
+                            if (status === 'OK') {
+                                map.fitBounds(bounds.extend(results[0].geometry.location));
+                                markersArray.push(new google.maps.Marker({
+                                    map: map,
+                                    position: results[0].geometry.location,
+                                }));
+                            } else {
+                                console.error('Geocode was not successful due to: ' + status);
+                            }
+                        };
+                    };*/
+
+                    for (var i = 0; i < originList.length; i++) {
+                        var results = response.rows[i].elements;
+                        /*geocoder.geocode({ 'address': originList[i] },
+                            showGeocodedAddressOnMap());*/
+                        for (var j = 0; j < results.length; j++) {
+                            /*geocoder.geocode({ 'address': destinationList[j] },
+                                showGeocodedAddressOnMap());*/
+                            vue.dist = results[j].distance.value/1000;           // Проблема в том, что внутри функции this это не vue, поэтому пишу vue
+                            vue.time = results[j].duration.value/60;
+                            console.info('Расстояние получено: ', results[j].distance.value);
+                            console.info('Время получено: ', results[j].duration.value);
+                        };
+                    };
+                };
+                
+                //подсчёт стоимости
+                vue.cost = vue.reservation + vue.dist * vue.perKilomiters + vue.time * vue.perMinutes;
+                vue.dist = Math.ceil(vue.dist * 100) / 100;
+                vue.time = Math.ceil(vue.time * 100) / 100;
+                vue.cost = Math.ceil(vue.cost * 100) / 100;
+                if (vue.cost < 1.65) {
+                    vue.cost = 1.65;
+                };
+                console.info('Стоимость посчитана ', vue.cost);
+            });
+        },
+        getDistance() {
+
         },
     }
 });
 
-var calcCost = document.querySelector('.calcCost');
-var directionsDisplay;
-var map;
 
-function initMap() {
-    directionsDisplay = new google.maps.DirectionsRenderer();
-    var minsk = new google.maps.LatLng(53.9, 27.56667);
-    var mapOptions = {
-        zoom: 11,
-        center: minsk,
+function initMap(cent) {
+    if(cent == undefined) {
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 11,
+            center: new google.maps.LatLng(53.9, 27.56667),
+        });
+        new google.maps.DirectionsRenderer().setMap(map);
+    } else {
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 11,
+            center: cent,
+        });
+        new google.maps.DirectionsRenderer().setMap(map);
     }
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    directionsDisplay.setMap(map);
-}
-
-calcCost.addEventListener('click', e => {
-    var start = document.getElementById('thirstAddress').value;
-    var end = document.getElementById('secondAddress').value;
-    var directionsService = new google.maps.DirectionsService();
-
-    var request = {
-        origin: start,
-        destination: end,
-        travelMode: 'DRIVING'
-    };
-    directionsService.route(request, function (result, status) {
-        if (status == 'OK') {
-            directionsDisplay.setDirections(result);
-        }
-    });
-
-    var service = new google.maps.DistanceMatrixService();
-    service.getDistanceMatrix(
-        {
-            origins: [start],
-            destinations: [end],
-            travelMode: 'DRIVING',
-        }, callback);
-});
-
-function callback(response, status) {
-    if (status == 'OK') {
-        var origins = response.originAddresses;
-        var destinations = response.destinationAddresses;
-
-        for (var i = 0; i < origins.length; i++) {
-            var results = response.rows[i].elements;
-            for (var j = 0; j < results.length; j++) {
-                var element = results[j];
-                var distanceText = element.distance.text;
-                vue.distValue = element.distance.value;
-                var durationText = element.duration.text;
-                vue.timeValue = element.duration.value;
-                var from = origins[i];
-                var to = destinations[j];
-            };
-        };
-        vue.dist = distanceText;
-        vue.time = durationText;
-        vue.calculateCost();
-        console.log(vue.distValue, vue.timeValue);
-    }
-}
+};
+console.info('initMap.js connected');
--- script.js
+++ script.js
@@ -1,51 +1,24 @@
-/*var calcCost = document.querySelector('.calcCost');
+function plusOne(num) {
+    console.log('START');
+    plusOrNot(num);
+};
 
-calcCost.addEventListener('click', e => {
-    var thirstAddress = document.getElementById('thirstAddress').value;
-    var secondAddress = document.getElementById('secondAddress').value;
+function plusOrNot(number) {
+    console.log('loading...')
+    return new Promise((resolve, reject) => {
+        if(Math.random() > 0.1) {
+            setTimeout(() => {
+                number++;
+                console.info('Number is ', number);
+                resolve(number);
+            }, 2000);
+        } else {
+            reject('Plus one stopped, number is equal ' + number);
+        };
+    });
+};
 
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 11,
-        center: { lat: 53.9, lng: 27.56667 },
-    });
-    /*geo = new google.maps.Geocoder();
-    geo.geocode({ 'address': thirstAddress }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            var home = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location,
-            });
-        }
-        else {
-            console.log('Not valid address');
-            console.log('results[0].geometry.location');
-        }
-    });
-    geo.geocode({ 'address': secondAddress }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            map.setCenter(results[0].geometry.location);
-            var job = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location,
-            });
-        }
-        else {
-            console.log('Not valid address');
-            console.log('results[0].geometry.location');
-        }
-    });*/
-    /*var directionsService = new google.maps.DirectionsService();
-    var directionsDisplay = new google.maps.DirectionsRenderer();
-    var request = {
-        origin: thirstAddress,
-        destination: secondAddress,
-        travelMode: 'DRIVING'
-    };
-    directionsService.route(request, function (result, status) {
-        if (status == 'OK') {
-            directionsDisplay.setDirections(result);
-        }
-    });
-    console.log(thirstAddress);
-    console.log(secondAddress);
-});*/
+
+plusOrNot(0)
+    .catch(err => console.error(err));
+
